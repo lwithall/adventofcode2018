b83e4f1343a66e873b2079054ae5b66cfd3f5aeb
Luke Withell <lukewithell@hotmail.com>
2019-01-05 12:55:41 +1100

    Making the code simplier

--- python/day6/p1/script.py
+++ python/day6/p1/script.py
@@ -2,11 +2,11 @@
 import operator
 
 def dist(x1, y1, x2, y2):
-    return abs(x1 - x2) + abs(y1 - y2)
+    return abs(x2 - x1) + abs(y2 - y1)
 
 def getClosest(x, y, points):
     distances = [(dist(x, y, points[i][0], points[i][1]), i) for i in range(len(points))]
-    sorted_distances = sorted(distances, key=lambda x: x[0])
+    sorted_distances = sorted(distances)
 
     if sorted_distances[0][0] == sorted_distances[1][0]:
         return -1
@@ -16,23 +16,21 @@ with open("../input.txt") as f:
 
     # Load points
     points = []
+    x_max = 0
+    y_max = 0
     for line in f:
         l = line.split(', ')
-        points.append((int(l[0]), int(l[1])))
-
-    x_max = points[0][0]
-    y_max = points[0][1]
-    for p in points:
-        x_max = max(x_max, p[0])
-        y_max = max(y_max, p[1])
+        x, y = int(l[0]), int(l[1])
+        points.append((x, y))
+        x_max = max(x_max, x)
+        y_max = max(y_max, y)
 
     counts = {}
     for point in range(len(points)):
         counts[point] = 0
 
-    for y in range(0, y_max+1):
-        line = ''
-        for x in range(0, x_max+1):
+    for x in range(x_max):
+        for y in range(y_max):
             i = getClosest(x, y, points)
             if i != -1:
                 counts[i] += 1
@@ -44,5 +42,6 @@ with open("../input.txt") as f:
     for c in range(len(counts)):
         if isFinite(points[c]):
             max_area = max(max_area, counts[c])
+
     print(counts)
     print(max_area)
