eec82484cfd37f53f9d7ce2d2c742f074c8263ae
Luke Withell <lukewithell@hotmail.com>
2019-01-05 12:42:51 +1100

    Another solution

--- python/day6/p1/script.py
+++ python/day6/p1/script.py
@@ -1,9 +1,6 @@
 
 import operator
 
-elements_upper = "ABCDEFGHIJKLMNOP"
-elements = "abcdefghijklmnop"
-
 def dist(x1, y1, x2, y2):
     return abs(x1 - x2) + abs(y1 - y2)
 
@@ -23,30 +20,25 @@ with open("../input.txt") as f:
         l = line.split(', ')
         points.append((int(l[0]), int(l[1])))
 
-    x_min = points[0][0]
     x_max = points[0][0]
-    y_min = points[0][1]
     y_max = points[0][1]
     for p in points:
-        x_min = min(x_min, p[0])
         x_max = max(x_max, p[0])
-        y_min = min(y_min, p[1])
         y_max = max(y_max, p[1])
 
     counts = {}
     for point in range(len(points)):
         counts[point] = 0
 
-    lines = []
-    for y in range(y_min-1, y_max+1):
-        line = str(y) + ' '
-        for x in range(x_min-1, x_max+1):
+    for y in range(0, y_max+1):
+        line = ''
+        for x in range(0, x_max+1):
             i = getClosest(x, y, points)
             if i != -1:
                 counts[i] += 1
 
     def isFinite(point):
-        return x_min < point[0] < x_max and y_min < point[1] < y_max
+        return 0 < point[0] < x_max and 0 < point[1] < y_max
 
     max_area = -1
     for c in range(len(counts)):
